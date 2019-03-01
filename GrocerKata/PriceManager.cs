e20a964106e62f7d669277d9d28d20231ffe4b36
Hunter Lind <>
2019-03-01 15:36:32 -0600

    Created pricemanager and added method UpdateItemPrice -- created test method UpdateItemPrice -- it is currently red.

---
+++ GrocerKata/PriceManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrocerKata
+{
+    public class PriceManager
+    {
+        public void UpdateItemPrice(string itemID, decimal price)
+        {
+
+        }
+    }
+}
---
+++ Kata_Tests/PriceManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrocerKata;
+
+namespace Kata_Tests
+{
+    [TestClass]
+    public class PriceManagerTests
+    {
+        PriceManager priceMan;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            priceMan = new PriceManager();
+        }
+
+        [TestMethod]
+        public void UpdateItemPrice()
+        {
+            decimal newPrice = 6.99m;
+            priceMan.UpdateItemPrice("hamburger", newPrice);
+            Assert.AreEqual(6.99m, FakeInventory.Inventory["hamburger"]);
+        }
+    }
+}
