f7f6f48e7007954042a6872b05a1e6c969c51520
Hunter Lind <>
2019-03-01 14:44:19 -0600

    Added method to test adding an item to the cart. It is red.

--- GrocerKata/Cart.cs
+++ GrocerKata/Cart.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GrocerKata
 {
     public class Cart
     {
-        
+        public Dictionary<string, int> Items = new Dictionary<string, int>();
+        public void AddItem(string itemID)
+        {
+
+        }
     }
 }
--- Kata_Tests/CartTests.cs
+++ Kata_Tests/CartTests.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrocerKata;
 
 namespace Kata_Tests
 {
     [TestClass]
-    public class PriceTests
+    public class CartTests
     {
+        Cart testCart;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            testCart = new Cart();
+        }
+
         [TestMethod]
         public void AddItemToCart()
         {
-            
+            testCart.AddItem("apple");
+            Assert.AreEqual(1, testCart.Items["apple"]);
         }
     }
 }
