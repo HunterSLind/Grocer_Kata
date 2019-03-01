0fd63e169d2f5687af97602667c756c0f3c1e6aa
Hunter Lind <>
2019-03-01 15:59:20 -0600

    Added new class pricecalculator, added method to said class, along with test method PriceCalculator.CalculateCartPrice(Cart) PriceCalculatorTests.PriceEmptyCart -- RED

---
+++ GrocerKata/PriceCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrocerKata
+{
+    public static class PriceCalculator
+    {
+        public static decimal CalculateCartPrice(Cart cart)
+        {
+            return -1;
+        }
+    }
+}
---
+++ Kata_Tests/PriceCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GrocerKata;
+
+namespace Kata_Tests
+{
+    [TestClass]
+    public class PriceCalculatorTests
+    {
+        Cart cart;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            cart = new Cart();
+        }
+        
+        [TestMethod]
+        public void PriceEmptyCart()
+        {
+            Assert.AreEqual(0.00m, PriceCalculator.CalculateCartPrice(cart));
+        }
+    }
+}
