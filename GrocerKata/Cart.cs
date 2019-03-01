f053a7563b8cb094bfc220873167b6df77becd08
Hunter Lind <>
2019-03-01 14:49:31 -0600

    Wrote minimal passing code for AddItemsToCart which is now green.

--- GrocerKata/Cart.cs
+++ GrocerKata/Cart.cs
@@ -8,7 +8,14 @@ namespace GrocerKata
         public Dictionary<string, int> Items = new Dictionary<string, int>();
         public void AddItem(string itemID)
         {
-            Items.Add(itemID, 1);
+            if (!Items.ContainsKey(itemID))
+            {
+                Items.Add(itemID, 1);
+            }
+            else
+            {
+                Items[itemID] += 1;
+            }
         }
     }
 }
